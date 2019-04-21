c36d4bc7f77c37c587829a5e863c49fd85f99dde
javimayer14 <javimayer14@gmail.com>
2019-04-21 20:14:31 -0300

    entidad usuario actualizada

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
-INSERT INTO delegado (id_empresa,dni,nombre, apellido,telefono,telefono_celular,email,sindicato,descripcion,antiguedad,superado,eliminado) values (1,2456790,'juan','lopez',42345432,14234234,'lala@lala.com','uom','es un buen tipo',8,0,0);
 INSERT INTO variacion_laboral (id_delegado,id_tipo_contrato,id_motivo,cantidad, tipo_variacion,tipo_incorporacion,descripcion) values (1,2,3,2,'lala','lala','aweaweawelala.com');
-INSERT INTO conflicto_laboral (id_conflicto,id_delegado,motivo, medida,descripcion) values (1,2,'lala','lala','aweaweawelala.com');
+INSERT INTO conflicto_laboral (id_conflicto,id_delegado,motivo, medida,descripcion) values (1,2,'lala','lala','aweaweawelala.com'); 

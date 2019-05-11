bee6fc0dd554c562e4066a8dc47536cea91d8b94
javimayer14 <javimayer14@gmail.com>
2019-05-11 18:37:08 -0300

    commit

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO variacion_laboral (id_delegado,id_tipo_contrato,id_motivo,cantidad, tipo_variacion,tipo_incorporacion,descripcion) values (1,2,3,2,'lala','lala','aweaweawelala.com');
-INSERT INTO conflicto_laboral (id_conflicto,id_delegado,motivo, medida,descripcion) values (1,2,'lala','lala','aweaweawelala.com'); 
